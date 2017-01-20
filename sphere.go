ab59ca45da04d3a52943eaa197a586bdb7569fc5
Lukas Redynk <mr.erdk@gmail.com>
2017-01-20 15:38:57 +0100

    Serializing scene to json
    In world and materials start member name with captial letter to expose it to the json marshaller.
    
    Prep work for enabling profiling with pprof and for rendering from json file instead of scene generated on-the-fly.

--- sphere.go
+++ sphere.go
@@ -7,16 +7,16 @@ import (
 )
 
 type sphere struct {
-	c mgl64.Vec3
-	r float64
-	m material
+	Center   mgl64.Vec3
+	Radius   float64
+	Material material
 }
 
 func (s *sphere) calcHit(r *ray, tMin, tMax float64) (bool, hit) {
-	oc := r.origin.Sub(s.c)
+	oc := r.origin.Sub(s.Center)
 	a := r.direction.Dot(*r.direction)
 	b := oc.Dot(*r.direction)
-	c := oc.Dot(oc) - s.r*s.r
+	c := oc.Dot(oc) - s.Radius*s.Radius
 
 	discriminant := b*b - a*c
 	if discriminant > 0 {
@@ -26,9 +26,9 @@ func (s *sphere) calcHit(r *ray, tMin, tMax float64) (bool, hit) {
 			var rec hit
 			rec.t = temp
 			rec.p = r.pointAtParam(rec.t)
-			rec.n = rec.p.Sub(s.c)
-			rec.n = rec.n.Mul(1.0 / s.r)
-			rec.m = s.m
+			rec.n = rec.p.Sub(s.Center)
+			rec.n = rec.n.Mul(1.0 / s.Radius)
+			rec.m = s.Material
 			return true, rec
 		}
 
@@ -37,9 +37,9 @@ func (s *sphere) calcHit(r *ray, tMin, tMax float64) (bool, hit) {
 			var rec hit
 			rec.t = temp
 			rec.p = r.pointAtParam(rec.t)
-			rec.n = rec.p.Sub(s.c)
-			rec.n = rec.n.Mul(1.0 / s.r)
-			rec.m = s.m
+			rec.n = rec.p.Sub(s.Center)
+			rec.n = rec.n.Mul(1.0 / s.Radius)
+			rec.m = s.Material
 			return true, rec
 		}
 	}
